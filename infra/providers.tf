aba60ee4a57368e596915369ce56c3eddb0dd98b
Miki Eto <mikieto@gmail.com>
2025-05-16 05:48:15 +0000

    refactor(ci): finish workflow rewrite
    * Refactor CI workflows to enhance security and efficiency.
    * Update GitHub Actions workflows to use reusable workflows.

--- infra/providers.tf
+++ infra/providers.tf
@@ -8,10 +8,5 @@ terraform {
 }
 
 provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "dummy"
-  secret_key                  = "dummy"
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
+  region = "us-east-1"
 }
